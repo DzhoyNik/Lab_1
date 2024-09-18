00e1b6b3ef40e5a056258e1c2ed5618005fa3804
DzhoyNik <57442310+DzhoyNik@users.noreply.github.com>
2024-09-18 17:29:12 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿/**********************
+* Автор: Михалел А.А. *
+* Дата: 17.09.2024    *
+* Название: лаба 1    *
+**********************/
+
+
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+int main() {
+
+	double sideA = 3.847;
+	double sideB = 5.33;
+	double sideC = 7.167;
+	float angleA, angleB, angleC, angleCChecked, p;
+
+	angleA = acos((pow(sideB, 2) + pow(sideC, 2) - pow(sideA, 2)) / (2 * sideB * sideC));
+	angleB = asin((sideB * sin(angleA)) / sideA);
+	angleC = M_PI - angleA - angleB;
+	p = (sideA + sideB + sideC) / 2;
+	angleCChecked = 2 * atan(sqrt(((p - sideA) * (p - sideB)) / (p * (p - sideC))));
+
+	if (angleC == angleCChecked) {
+		cout << angleA << "; " << angleB << "; " << angleC;
+	}
+}
